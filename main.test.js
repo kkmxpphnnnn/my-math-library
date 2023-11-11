1fbb87dbf61713922aba5aed165824368bc11b4a
kmmxx phann <fanta500hahaha@gmail.com>
2023-11-11 11:10:39 +0700

    Initial the math library

--- main.test.js
+++
@@ -1,4 +0,0 @@
-const math = requrie('./math')
-const assert = requrie('assert')
-
-assert.strictEqual(math.sum(2, 2), 4, 'should sum two numbers')
---
+++ math.js
@@ -0,0 +1,10 @@
+// 📄 math.js
+
+function sum(a, b) {
+  return a + b
+}
+
+module.exports = {
+  sum,
+}
+
