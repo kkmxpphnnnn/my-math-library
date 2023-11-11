8c850739190e9c064a15eeaf96d2986ebe4de426
kmmxx phann <fanta500hahaha@gmail.com>
2023-11-11 11:17:26 +0700

    Add sum array funtionarray

--- math.js
+++ math.js
@@ -1,10 +1,23 @@
 // 📄 math.js
 
 function sum(a, b) {
-  return a + b
+  if (Array.isArray(a)) {
+    let total = 0
+    for (let i = 0; i < a.length; i++) {
+      total += a[i]
+    }
+    return total
+  } else if (typeof a === 'number' && typeof b === 'number') {
+    return a + b
+  }
 }
+// ... module.export ...
+
+
+
 
 module.exports = {
   sum,
 }
+
 
