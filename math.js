7b4f96c7919ce53fb2ec1786349a4ae1fddbf530
kmmxx phann <fanta500hahaha@gmail.com>
2023-11-11 11:35:58 +0700

    Use array for each

--- math.js
+++ math.js
@@ -1,16 +1,21 @@
 // 📄 math.js
 
+// 📄 math.js
+
 function sum(a, b) {
   if (Array.isArray(a)) {
-    let total = 0
-    for (let i = 0; i < a.length; i++) {
-      total += a[i]
-    }
-    return total
+    let sum = 0
+    a.forEach((i) => (sum += i))
+    return sum
   } else if (typeof a === 'number' && typeof b === 'number') {
     return a + b
   }
 }
+
+
+
+
+
 // ... module.export ...
 
 
